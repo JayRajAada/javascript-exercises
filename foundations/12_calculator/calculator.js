c4dc9ad4c45bf027d7ef0ba0d2169aa0db195f22
JayRajAada <jayrajaada@gmail.com>
2026-05-18 18:54:13 +0530

    Done foundations from question 12 to 16.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total , item)=>total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total , item)=>total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  fact = 1;
+	while(a != 1){
+    fact = fact*a;
+    a--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count;
+    if (typeof n !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = n
+    }
+    if (isNaN(count)) return "OOPS";    
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+    if (count == 1) return 1;
+    
+    let firstPrev = 1;
+    let secondPrev = 0;
 
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
